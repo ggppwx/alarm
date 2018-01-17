dcf0450bf6f2a5ec4beadef5fad48582ce167c10
roy gu <sirgujingwei@gmail.com>
2018-01-16 22:24:36 -0500

    update

--- alarm.py
+++ alarm.py
@@ -22,37 +22,46 @@ class Org(object):
         for f in self._files:
             nodelist = Orgnode.makelist(f)
             for n in nodelist:
-                if n.Todo() != 'DONE' and n.Scheduled() == date:
+                if (n.Todo() != 'DONE'
+                    and n.Scheduled() and str(n.Scheduled()) <= str(date)):
+                    print n.Scheduled()
+                    print n.Heading()
                     tasks.append(n.Heading())
         return tasks
 
 
 class Job(object):
-    def __init__(self):
+    def __init__(self, org_file):
         self._system = platform.system()
-        self._org = Org(['/Users/jingweigu/Dropbox/org/scratch.org'])
+        self._org = Org([org_file])
 
     def _speak(self, text):
         if self._system == 'Darwin':
-            os.system('say {}'.format(text))
-        else:                
+            os.system('say "{}"'.format(text))
+        else:
             os.system('espeak -s 150 -a 200 "{}"'.format(text))
 
-    def _report_time(self, sound ):
-        playsound(sound)
-
-    def _read_todays_org(self):
-        self._speak('Todays task has: ')
-        today = datetime.date.today() 
-        tasks = self._org.getTasks(today)
-        for task in tasks:
-            self._speak(task)
-            time.sleep(1.5)
-
-
-    def run(self, text, sound):
+    def _report_time(self, sound, text):
         playsound(sound)
         self._speak(text)
+
+    def _read_todays_org(self):
+        today = datetime.date.today() 
+        tasks = self._org.getTasks(today)
+        if len(tasks) == 0:
+            self._speak('No task today')
+        else:
+            self._speak('Todays task has: ')
+            for task in tasks:
+                self._speak(task)
+                time.sleep(1.5)
+
+
+    def run(self, text, sound, org):
+        self._report_time(sound, text)
+        time.sleep(2)
+        if org:
+            self._read_todays_org()
 
 
 def main():
@@ -70,18 +79,25 @@ def main():
     with open(args.config) as configFile:
         config.readfp(configFile)
 
-
-    job = Job()
+    org_file = config.get('setting', 'orgfile')
+    job = Job(org_file)
 
     for key, value in config.items('time'):
         sound = 'music.mp3'
         if config.has_option('sound', key):
             sound = config.get('sound', key)
 
+        report_org = False
+        if config.has_option('org', key):
+            report_org = config.getboolean('org', key)
+
         print sound
         print value
         # adding system alarm
-        schedule.every().day.at(value).do(job.run, text=str(value), sound=str(sound))
+        schedule.every().day.at(value).do(job.run,
+                                          text=str(value),
+                                          sound=str(sound),
+                                          org=report_org)
 
     while 1:
         schedule.run_pending()
@@ -90,13 +106,14 @@ def main():
 
 
 def test():
-    job = Job()
+    job = Job('/Users/jingweigu/Dropbox/org/scratch.org')
     job._speak('11')
 
 def testOrg():
-    job = Job()
+    job = Job('/Users/jingweigu/Dropbox/org/scratch.org')
     job._read_todays_org()
 
 if __name__ == "__main__":
-    testOrg()
+    main()
+    #testOrg()
 
